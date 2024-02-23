064ad567be59d636e9ea7f123d74e7b39e1084b1
Jorge Isaí García Reyes <isai_18k@hotmail.com>
2024-02-23 12:05:49 -0600

    feat: Some design elements are added to the main character list
    BREAKING CHANGE:
    The list is deleted and converted to a grid.
    Contours and colors are added to the grid.

--- RickAndMortyApp/RickAndMortyApp/CharacterListView.swift
+++ RickAndMortyApp/RickAndMortyApp/CharacterListView.swift
@@ -14,66 +14,96 @@ struct CharacterListView: View {
     @StateObject var viewModel = CharacterListViewModel()
     @State private var showMenu = false
     @State private var isSearchViewPresented = false
+    
+
+    let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
 
     var body: some View {
         NavigationView {
-            List(viewModel.characters) { character in
-                NavigationLink(destination: CharacterDetailView(character: character)) {
-                    HStack {
-                        KFImage(URL(string: character.image)) // Use KFImage instead of AsyncImage
-                            .placeholder{
-                                Image(systemName: "person.fill")
-                                                        .resizable()
-                                                        .aspectRatio(contentMode: .fit)
-                                                        .frame(width: 50, height: 50)
-                                                        .foregroundColor(.gray)
+            ZStack {
+               
+                Image("RaMBackGround2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                ScrollView {
+                    LazyVGrid(columns: gridItems, spacing: 16) {
+                        ForEach(viewModel.characters) { character in
+                            NavigationLink(destination: CharacterDetailView(character: character)) {
+                                VStack {
+                                    KFImage(URL(string: character.image))
+                                        .placeholder {
+                                            Image(systemName: "person.fill")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 50, height: 50)
+                                                .foregroundColor(.gray)
+                                        }
+                                        .onFailure { error in
+                                            print("Image failed to load: \(error)")
+                                        }
+                                        .fade(duration: 0.25)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 100, height: 100)
+                                        .clipShape(Circle())
+                                        .overlay(
+                                            Circle()
+                                            .stroke(Color.green, lineWidth: 2) // Green border
+                                        )
+                                    
+                                    Text(character.name)
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 18))
+                                        .lineLimit(2)
+                                        .padding(.horizontal, 8)
+                                        .multilineTextAlignment(.center)
+                                        .fixedSize(horizontal: false, vertical: true) // Allow text
+                                }
+                                .frame(width: 140, height: 150)
+                                .background(Color.black.opacity(0.70))
+                                .cornerRadius(10)
+                                .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.green, lineWidth: 2)
+                                )
                             }
-                            .onFailure { error in
-                                print("Image failed to load: \(error)")
-                            }
-                            .fade(duration: 0.25) // Add fade transition
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 50, height: 50)
-                            .clipShape(Circle())
-                                           
-                        Text(character.name)
+                        }
                     }
+                    .padding()
                 }
-            }
-            .navigationTitle("Rick and Morty Characters")
-            .navigationBarTitleDisplayMode(.inline)
-            .font(.footnote)
-            .toolbar {
+                .navigationTitle("Rick and Morty Characters")
+                .navigationBarTitleDisplayMode(.inline)
+                .font(.footnote)
+                .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
                         Menu {
                             Button(action: {
-                                // Action for Option 1
-                                isSearchViewPresented = true // Set the state variable to true to present the SearchView
+                                // Action for Search Character
+                                isSearchViewPresented = true
                             }) {
                                 Label("Search Character", systemImage: "magnifyingglass")
                             }
                             Button(action: {
-                                // Action for Option 2
+                                // Action for Option About
                             }) {
                                 Label("About App", systemImage: "flame")
                             }
                         } label: {
-                                Image(systemName: "ellipsis.circle")
+                            Image(systemName: "ellipsis.circle")
                         }
                     }
+                }
             }
             .sheet(isPresented: $isSearchViewPresented) {
-                // Present the CharacterSearchView when isSearchViewPresented is true
                 CharacterSearchView()
             }
-            
         }
         .onAppear {
             viewModel.fetchCharacters()
         }
         .alert(isPresented: $showMenu) {
-                Alert(title: Text("Options"), message: nil, dismissButton: .default(Text("OK")))
+            Alert(title: Text("Options"), message: nil, dismissButton: .default(Text("OK")))
         }
     }
 }
@@ -119,7 +149,7 @@ struct CharacterSearchView: View {
             }
         }
 
-        // Computed property to filter characters based on search query
+        // Filter characters based on search query
         private var filteredCharacters: [RMCharacterModel] {
             if searchText.isEmpty {
                 return viewModel.characters
